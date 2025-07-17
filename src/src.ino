6e92ed1b5c04979c2c1273c6e3c40565f7847a4b
henriqueedu2001 <henrique_eduardo_souza@hotmail.com>
2025-07-16 23:46:18 -0300

    test: computing the HMAC-SHA1 signature and validating it in the main loop

--- src/src.ino
+++ src/src.ino
@@ -2,11 +2,17 @@
 #include <qrcode.h>
 #include <decoder.h>
 #include <stdint.h>
+#include <auth.h>
 
 #include "esp_heap_caps.h"
 
 #define BAUD_RATE 115200
 #define LED_BUILTIN 4
+
+uint8_t DEFAULT_ACCESS_KEY[20] = {
+  0x85, 0xf1, 0xe2, 0x04, 0xba, 0x63, 0xfe, 0x41, 0xa0, 0xf0,
+  0xda, 0x37, 0x74, 0x3e, 0x8d, 0x1c, 0x6a, 0xf5, 0x33, 0xfc
+};
 
 void ledBlink(int n);
 void printHeapFreeSize();
@@ -24,6 +30,12 @@ void loop() {
   if(qrcodePayload.successfulRead) {
     DecodedQRCodeData decodedQRCodeData = getQRCodeData(qrcodePayload.rawPayload, qrcodePayload.payloadLength);
     printDecodedQRCodeData(decodedQRCodeData);
+    uint8_t outputHash[20];
+    getHMAC_SHA1(DEFAULT_ACCESS_KEY, KEY_LENGTH, decodedQRCodeData.payloadMessage, decodedQRCodeData.messageLength, outputHash);
+    bool signatureValidity = validateSignature(decodedQRCodeData.payloadHash, outputHash);
+    printHMACSHA1(outputHash);
+    Serial.print("Signature validity: ");
+    Serial.println(signatureValidity);
     freeMallocData(&decodedQRCodeData);
     ledBlink(1, 50);
   }
